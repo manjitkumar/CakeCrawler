f3dd9f6896f3fd8db81280bcfc2710a8d4597b04
Manjit Kumar <manav9316@users.noreply.github.com>
2014-04-04 21:20:58 +0530

    Create Cake.py
    Cake class

---
+++ Cake.py
@@ -0,0 +1,7 @@
+class Cake:
+    def __init__(self, name, url, price, desc, img_src):
+        self.name = name
+        self.url = url
+        self.price = price
+        self.desc = desc
+        self.img_src = img_src
